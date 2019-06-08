6b4153ee9296767e9f4d68bc9fbacea84952b95f
lambert.zhang <minger90@163.com>
2019-06-09 00:30:52 +0800

    footer

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,32 @@
 <template>
     <div id="app">
-        <router-view/>
+        <el-container>
+            <!--<el-header>Header</el-header>-->
+            <el-main>
+                <router-view/>
+            </el-main>
+            <el-footer>
+                <foot/>
+            </el-footer>
+        </el-container>
     </div>
 </template>
 
 <style lang="less">
     @import 'common/common.less';
+    @import './styles/app.less';
 </style>
+
+<script lang="ts">
+    import {Component, Vue} from "vue-property-decorator";
+    import foot from '@/components/foot.vue';
+
+    @Component({
+        components: {
+            foot
+        }
+
+    })
+
+    export default class Foot extends Vue {}
+</script>
---
+++ src/components/foot.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="foot">
+        <el-row>
+            <el-col :span="8">
+                111
+            </el-col>
+            <el-col :span="8">
+                222
+            </el-col>
+            <el-col :span="8">
+                333
+            </el-col>
+        </el-row>
+    </div>
+</template>
+
+<style lang="less">
+    @import '../styles/foot.less';
+</style>
+
+<script lang="ts">
+
+</script>
