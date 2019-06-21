3d836ac1ca46ee5a234b9b745d88a8bd309137eb
v_zhangming05 <v_zhangming05@baidu.com>
2019-06-21 10:23:45 +0800

    detail page

--- src/components/homeCard.vue
+++ src/components/homeCard.vue
@@ -2,13 +2,14 @@
     <div class="card-list">
         <el-card class="card-list-item"
                  v-for="item in cardList"
-                 @click="routerGo(item.url)"
                  :key="item.id">
-            <img class="card-list-item-image" :src=item.imgSrc>
+            <img class="card-list-item-image"
+                 :src=item.imgSrc
+                 @click="routerGo(item.url)">
             <div class="card-list-item-title">{{item.title}}</div>
             <div class="card-list-item-other clearfix">
                 <time class="card-list-item-time">{{ item.time }}</time>
-                <span class="card-list-item-money">${{item.money}}</span>
+                <span class="card-list-item-money">￥{{item.money}}</span>
             </div>
         </el-card>
     </div>
@@ -16,30 +17,6 @@
 
 <style lang="less" scoped>
     @import '../styles/homeCard.less';
-    .card-list {
-        &-item {
-            margin-bottom: 20px;
-            &-image {
-                display: block;
-                width: 100%;
-            }
-            &-title {
-                padding: 10px 0;
-            }
-            &-other {
-                position: relative;
-            }
-            &-money {
-                position: absolute;
-                top: 0;
-                right: 0;
-                background: #fff;
-            }
-            &-time {
-                width: 100%;
-            }
-        }
-    }
 </style>
 
 <script lang="ts">
---
+++ src/views/detail.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        detail page
+    </div>
+</template>
+
+<style lang="less" scoped>
+    @import "../styles/detail.less";
+</style>
+
+<script lang="ts">
+    import {Vue, Component, Prop} from 'vue-property-decorator';
+
+    export default class detail extends Vue{
+
+    }
+</script>
